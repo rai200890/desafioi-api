c18d194727439c418531b36250bb0a4c54d08f0d
Raissa Ferreira <rai200890@gmail.com>
2016-09-05 01:03:49 -0300

    Add issue reason endpoint
    - Add missing state endpoint tests

--- spec/controllers/states_controller_spec.rb
+++ spec/controllers/states_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe StatesController, type: :controller do
-
+  before :each do
+    FactoryGirl.create_list(:state, 5)
+    get :index
+  end
+  it 'should return a list of states' do
+    json = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(json['states'].first.keys).to include('abbreviation', 'name')
+    expect(json['states'].length).to eq(5)
+  end
 end
