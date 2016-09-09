d15394ae87e49b823e0b0d58447fe73da7704efe
Raissa Ferreira <rai200890@gmail.com>
2016-09-08 21:55:01 -0300

    Add issue model scope test

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -16,4 +16,22 @@ describe 'validations' do
   it { expect(issue).to validate_presence_of(:customer) }
 end
 
+describe '.group_by_date_and_state' do
+  let(:state1){FactoryGirl.create(:state, abbreviation: 'RJ', name: 'Rio de Janeiro')}
+  let(:state2){FactoryGirl.create(:state, abbreviation: 'SP', name: 'São Paulo')}
+  let(:state3){FactoryGirl.create(:state, abbreviation: 'MG', name: 'Minas Gerais')}
+  let!(:issue1){FactoryGirl.create(:issue, state: state2, created_at: Time.now - 5.hour)}
+  let!(:issue2){FactoryGirl.create(:issue, state: state2, created_at: Time.now - 2.hour)}
+  let!(:issue3){FactoryGirl.create(:issue, state: state1, created_at: Time.now - 3.hour)}
+  let!(:issue4){FactoryGirl.create(:issue, state: state3, created_at: Time.now - 4.hour)}
+  it 'should return issues grouped by state and ordered by date desc' do
+    expect(Issue.group_by_date_and_state).to eq([
+      issue4,
+      issue3,
+      issue2,
+      issue1
+      ])
+  end
+end
+
 end
